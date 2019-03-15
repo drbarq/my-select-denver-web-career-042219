9e22c3f4878b6ad3c9b52fa6ee2be689e51c9550
Joe Tustin <j.tustin@gmail.com>
2019-03-15 22:08:20 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
+
+
 def my_select(collection)
- # your code here!
+  i = 0
+  return_array = Array.new
+  while i < collection.length do
+    if yield(collection[i])
+      return_array << collection[i]
+      i = i + 1
+    else
+      i = i + 1
+  end
+  end
+  return return_array
 end
+
+
+
+ # your code here!
